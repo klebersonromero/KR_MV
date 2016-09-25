50981344f43e43176d805fb177cf211f6c7dbde3
Kleberson Romero <klebersonromerotj@gmail.com>
2016-09-25 11:00:38 -0300

    Add files via upload

---
+++ Plugins/Padrao Plugins.js
@@ -0,0 +1,13 @@
+/*:
+ *
+ *@plugindesc Descrição do Plugin
+ *@author Komuro Takashi
+ *
+ *@param Text
+ *@desc escreve seu Texto
+ *@deafault Texto Padrao
+ *
+ *@help
+ *Texto para ajudar o usuário a usar o plugin
+ *
+*/
